43e6b61f50b3acc55b89df7aaa874b3f0b6f542e
Gurk1992 <31844868+Gurk1992@users.noreply.github.com>
2018-11-26 23:10:43 +0100

    Add files via upload

--- sem3/classes/Comment/Integration/mysql.php
+++ sem3/classes/Comment/Integration/mysql.php
@@ -4,7 +4,7 @@ class mysql{
  private $conn = null;
 
 public function __construct(){
-     $this->conn= new mysqli("localhost","root","cjopg123","myDB");
+     $this->conn= new mysqli("localhost","root","123","myDB");
 // check connection
 if($this->conn->connect_error){
     die("Connection failed: " . $this->conn->connect_error);
@@ -19,26 +19,42 @@ if($this->conn->connect_error){
     return $query;
    
  }
+ /**
+  * Checks if username is in db,
+  * returns true / false, true when info matches correctly otherwise no.
+  */
  public function login($sql, $username){
-    // $stmt = $this->conn->prepare($sql);
      $stmt = $this->conn->prepare($sql);
      $stmt-> bind_param('s', $username);
      $stmt->execute();
      return $stmt->get_result();
    
  }
+ /**
+  * Inserts account name and password in db
+  * returns true / false, true when info is saved correctly otherwise no.
+  */
+ 
  public function register($sql, $username, $password){
     $stmt = $this->conn->prepare($sql);
     $stmt-> bind_param('ss', $username, $password);
     return $stmt->execute();
     
  }
+ /**
+  * Inserts comment text, username, and what recept id into db
+  * returns true / false, true when info is saved correctly otherwise no.
+  */
  public function comment($sql, $receptId, $username,$com){
     $stmt = $this->conn->prepare($sql);
      $stmt-> bind_param('sss', $receptId, $username, $com);
      return $stmt->execute();
     
  }
+ /**
+  * Deletes comment that matches postid in db.
+  * returns true / false, true when comment is deleted correctly otherwise no.
+  */
  public function deleteComment ($sql, $postid){
     $stmt = $this->conn->prepare($sql);
     $stmt-> bind_param('s', $postid);
